c2e92592cf8cbe17f871ef1a588d422f518edb8f
akshaypatole17 <akshaypatole17@gmail.com>
2019-07-07 14:43:49 +0530

    PFA

---
+++ GinningERP/src/com/prov/bean/WeighRateMast.java
@@ -0,0 +1,36 @@
+package com.prov.bean;
+
+public class WeighRateMast {
+	
+	int id;
+	String vehicle_name, vehcile_Desc;
+	double weighrate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getVehicle_name() {
+		return vehicle_name;
+	}
+	public void setVehicle_name(String vehicle_name) {
+		this.vehicle_name = vehicle_name;
+	}
+	public String getVehcile_Desc() {
+		return vehcile_Desc;
+	}
+	public void setVehcile_Desc(String vehcile_Desc) {
+		this.vehcile_Desc = vehcile_Desc;
+	}
+	public double getWeighrate() {
+		return weighrate;
+	}
+	public void setWeighrate(double weighrate) {
+		this.weighrate = weighrate;
+	}
+	
+	
+	
+
+}
---
+++ GinningERP/src/com/prov/dbinsertion/AddWeighRateMast.java
@@ -0,0 +1,53 @@
+package com.prov.dbinsertion;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import com.prov.bean.WeighRateMast;
+import com.prov.db.OracleConnection;
+
+public class AddWeighRateMast {
+
+public int addWeighRateMast(WeighRateMast wrm) {
+		
+		Connection con = null;
+		int id = 0;
+		try {
+			con = OracleConnection.getConnection();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		String addWeighRateMast = "{ ? = call ADD_WEIGHRATEMAST(?,?,?) }";
+		CallableStatement cs;
+		try {
+			cs = con.prepareCall(addWeighRateMast);
+			
+			cs.registerOutParameter(1, Types.NUMERIC);
+			
+			cs.setString(2, wrm.getVehicle_name() );
+			cs.setString(3, wrm.getVehcile_Desc() );
+			cs.setDouble(4, wrm.getWeighrate());
+			
+			cs.executeUpdate();
+			
+			id = cs.getInt(1);
+			
+			wrm.setId(id);
+			
+			cs.close();
+			con.close();
+			
+			System.out.println("Insertion Succesful"+id);
+			} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return id;
+	}
+
+
+}
+
